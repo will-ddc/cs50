bddeaeb0291441a4997f907035c8fae581006b67
Will Aronow <waronow123@gmail.com>
2020-06-02 13:41:26 -0700

    commented out for loop and called new cough function

--- week1-into-to-c/cough0.c
+++ week1-into-to-c/cough0.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
+  /*
+  for loop to print cough 3 times
   for (int i = 0; i < 3; i++) {
     printf("cough\n");
   }
+  */
+  cough();
+  cough();
+  cough();
 }
+
+// custom function to print cough
 
 void cough(void) {
   printf("cough\n");
