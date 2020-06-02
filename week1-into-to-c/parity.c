ca0d641e7fafec3d3df5478c2f67dcdeea2cdd8e
Will Aronow <waronow123@gmail.com>
2020-06-02 13:25:59 -0700

    added comments to parity file

--- week1-into-to-c/parity.c
+++ week1-into-to-c/parity.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 
 int main(void) {
+  // prompt user to input a number
+
   int n = get_int("Input a number\n");
+  /*
+  loop to check if input number is even or odd
+   */
   if (n % 2 == 0)
   {
     printf("%i is even\n", n);
