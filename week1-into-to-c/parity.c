d4e82ec0a5f5374cea15251c0ab97e2f8f6a88bd
Will Aronow <waronow123@gmail.com>
2020-06-02 13:17:37 -0700

    created parity file to compare input num to see if its even/odd

---
+++ week1-into-to-c/parity.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+  int n = get_int("Input a number\n");
+  if (n % 2 == 0)
+  {
+    printf("%i is even\n", n);
+  }
+  else
+  {
+    printf("%i is odd\n", n);
+  }
+}
