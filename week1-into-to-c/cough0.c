eefc1b65e62e8ca71e4d202b408640c5f7bac9d5
Will Aronow <waronow123@gmail.com>
2020-06-02 13:48:51 -0700

    moved cough function back below, added line above main function to fix error

--- week1-into-to-c/cough0.c
+++ week1-into-to-c/cough0.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-// custom function to print cough
+// List of Functions used in app
 
-void cough(void) {
-  printf("cough\n");
-}
+void cough(void);
+
+// Main App
 
 int main(void) {
   // for loop to print cough 3 times
   for (int i = 0; i < 3; i++) {
     cough();
   }
+}
+
+// custom function to print cough
+
+void cough(void) {
+  printf("cough\n");
 }
